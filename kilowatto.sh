838f59e5a51b0ab7b095c0b7be670f876579b266
ArturoLeveau <38760787+Tuury@users.noreply.github.com>
2024-10-03 14:25:01 -0600

    Add files via upload

---
+++ kilowatto.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiOTljOTMwMGYxNzVhZjBlNzY0ODNiOTQ5ZjZjNmFjZDEiLCJ0IjoiYmZhMTVjMDQtZmI4Ni00ZmE4LTg0ZjktY2I3ZjdiYTI4N2I5IiwicyI6Ik1USXpNVGcxWm1VdFpUVmxOUzAwWWpObExXSTFPV0l0TVROaE16UmhZVGxsTmpBMiJ9
